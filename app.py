3832006a2c634cf7b3835f9cf2c0a7fac4feadc6
s-logika <273270343+s-logika@users.noreply.github.com>
2026-05-24 23:04:31 +0530

    connected mysql database to flask

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
